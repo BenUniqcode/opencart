99225c2de688d51123009fe97d23fce330f7cdc9
Ben Wheeler <ben@uniqcode.com>
2018-04-18 22:44:20 +0100

    On a category page which has subcategories, show them as thumb images instead of boring text-only list.

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -141,10 +141,19 @@ class ControllerProductCategory extends Controller {
 					'filter_sub_category' => true
 				);
 
-				$data['categories'][] = array(
+				// 2018-04-18 BW Make thumbnail available
+				$subcat_data = array(
 					'name' => $result['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-					'href' => $this->url->link('product/category', 'path=' . $this->request->get['path'] . '_' . $result['category_id'] . $url)
+					'href' => $this->url->link('product/category', 'path=' . $this->request->get['path'] . '_' . $result['category_id'] . $url),
 				);
+				if ($result['image']) {
+					$image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+				} else {
+					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+				}
+				$subcat_data['thumb'] = $image;
+
+				$data['categories'][] = $subcat_data;
 			}
 
 			$data['products'] = array();
